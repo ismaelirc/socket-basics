a59064e4cc664ae22bd27d49a8a948f6181a8640
Ismael <ismaelirc@gmail.com>
2016-03-03 14:58:47 -0300

    Added Front-end to chat app

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ io.on('connection',function(socket){
 		
 		console.log('Message received: '+message.text);
 
-		socket.broadcast.emit('message',message)
+		socket.broadcast.emit('message',message);
+		
 	});
 
 	socket.emit('message',{
