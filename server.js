945cf50dada6d4dd7698d50707f63034bbedc9af
Ismael <ismaelirc@gmail.com>
2016-03-08 15:29:16 -0300

    Add commands in chat

--- server.js
+++ server.js
@@ -9,6 +9,35 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+//send currents users to provided socket
+function sendCurrentUsers(socket){
+
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if(typeof info == 'undefined'){
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function(socketId){
+		var userInfo = clientInfo[socketId];
+
+		if(info.room === userInfo.room){
+			users.push(userInfo.name);
+		}
+
+	});
+
+	socket.emit('message',{
+		name: 'System',
+		text: 'Current user: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+
+
+}
+
+
 io.on('connection',function(socket){
 	console.log('User Connected via socket.io!');
 
@@ -43,9 +72,16 @@ io.on('connection',function(socket){
 		
 		console.log('Message received: '+message.text);
 
-		message.timestamp = moment().valueOf();
-		//io.emit('message',message);
-		io.to(clientInfo[socket.id].room).emit('message',message);
+		if(message.text === '!users'){
+			sendCurrentUsers(socket);
+
+		}else{
+
+			message.timestamp = moment().valueOf();
+			//io.emit('message',message);
+			io.to(clientInfo[socket.id].room).emit('message',message);	
+		}
+		
 	});
 
 	socket.emit('message',{
